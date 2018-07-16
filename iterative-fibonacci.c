8d1daca0282fceb2f6744a5b793b3deb6cd447de
Amar <amarsgithub@outlook.com>
2018-07-16 11:22:42 -0400

    Change int to LLU

--- iterative-fibonacci.c
+++ iterative-fibonacci.c
@@ -4,9 +4,9 @@
 int main(int argc, char **argv)
 {
     int i;
-    int first_num = 0;
-    int second_num = 1;
-    int third_num = 0;
+    long long unsigned int first_num = 0;
+    long long unsigned int second_num = 1;
+    long long unsigned int third_num = 0;
 
     if (argc < 2)
     {
@@ -14,13 +14,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    printf("0. %d\n", first_num);
-    printf("1. %d\n", second_num);
-    
+    printf("0. %llui\n", first_num);
+    printf("1. %llui\n", second_num);
+
     for (i = 2; i <= atoi(argv[1]); i++)
     {
         third_num = first_num + second_num;
-        printf("%d. %d\n", i, third_num);
+        printf("%d. %llu\n", i, third_num);
         first_num = second_num;
         second_num = third_num;
     }
